38b9d0674ba63f6fae54697658bedc1f33fcfea5
Aurélie Fqt <70887548+AurelieFqt@users.noreply.github.com>
2020-11-09 11:08:42 +0100

    Add POO 5

--- Bicycle.php
+++ Bicycle.php
@@ -4,5 +4,19 @@ require_once 'Vehicle.php';
 
 class Bicycle extends Vehicle
 {
-    
+    // ------------------------POO5-----------------------
+
+    public function switchOn():bool
+    {
+        if($this->currentSpeed > 10){
+            $switch = true;
+        }else{
+            $switch = false;
+        }
+        return $switch;
+    }
+    public function switchOff():bool
+    {
+        return false;
+    }
 }
--- Car.php
+++ Car.php
@@ -64,4 +64,16 @@ class Car extends Vehicle
     {
         $this->energyLevel = $energyLevel;
     }
+
+    
+    // ------------------------POO5-----------------------
+
+    public function switchOn():bool
+    {
+        return true;
+    }
+    public function switchOff():bool
+    {
+        return false;
+    }
 }
